d704b3999b1cc3588026bcdeb4bb28713ea88457
Maryna Markova <markova.maryna@gmail.com>
2023-03-14 21:24:18 +0200

    add basic file structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MarynaMarkova/Go-Learning/tree/main/First_Site_2
+
+go 1.20
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/MarynaMarkova/Go-Learning/tree/main/First_Site_2/pkg/render"
+)
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r * http.Request){
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request){
+	render.RenderTemplate(w, "about.page.tmpl")
+}
+
+
